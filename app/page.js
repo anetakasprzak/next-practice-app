87cebe05dc0a084e9a42ef265b6f3d20f29ac942
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-03 11:17:19 +0100

    main page nav

--- app/page.js
+++ app/page.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>TEST</h1>
-    </main>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>HOME PAGE</h1>
+
+      <ul>
+        <li>
+          <Link href="/advice">Advice</Link>
+        </li>
+        <li>
+          <Link href="/inputs">Inputs</Link>
+        </li>
+        <li>
+          <Link href="/modal">Modal</Link>
+        </li>
+      </ul>
+    </div>
   );
 }

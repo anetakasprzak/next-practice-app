5b46a6e331c161751a700a340fe9f4e87e05a7a0
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-04 10:26:15 +0100

    main page basic styles

--- app/page.js
+++ app/page.js
@@ -2,17 +2,17 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-cyan-700">HOME PAGE 2</h1>
+    <div className="flex min-h-screen flex-col items-center  p-24 gap-5">
+      <h1 className="text-cyan-600">MY EXERCISES</h1>
 
-      <ul className="">
-        <li>
+      <ul className="flex flex-col gap-2 bg-gray-200 py-2 px-4 border-2 border-solid border-cyan-600">
+        <li className="hover:text-cyan-600">
           <Link href="/advice">Advice</Link>
         </li>
-        <li>
+        <li className="hover:text-cyan-600">
           <Link href="/inputs">Inputs</Link>
         </li>
-        <li>
+        <li className="hover:text-cyan-600">
           <Link href="/modal">Modal</Link>
         </li>
       </ul>

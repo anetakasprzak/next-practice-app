94ed00186719a5b5727e58ed33e43ee94066269e
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-04 11:38:36 +0100

    open/close modal functionality

--- app/modal/page.js
+++ app/modal/page.js
@@ -1,10 +1,24 @@
+"use client";
+
+import { useState } from "react";
 import GoBack from "../_components/GoBack";
 
 export default function Page() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div className="px-14 py-10">
       <GoBack />
-      <h1>MODAL PAGE</h1>
+      {!isModalOpen ? (
+        <button onClick={() => setIsModalOpen(true)}>open modal</button>
+      ) : (
+        <button onClick={() => setIsModalOpen(false)}>close modal</button>
+      )}
+      {isModalOpen && <Modal />}
     </div>
   );
 }
+
+const Modal = () => {
+  return <p>MODAL</p>;
+};

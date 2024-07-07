414b28a3dd6a62465bc20bcdcae8337040b76270
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-07 18:02:11 +0100

    modal styles

--- app/modal/page.js
+++ app/modal/page.js
@@ -14,11 +14,40 @@ export default function Page() {
       ) : (
         <button onClick={() => setIsModalOpen(false)}>close modal</button>
       )}
-      {isModalOpen && <Modal />}
+      {isModalOpen && <Modal setIsModalOpen={setIsModalOpen} />}
     </div>
   );
 }
 
-const Modal = () => {
-  return <p>MODAL</p>;
+const Modal = ({ setIsModalOpen }) => {
+  return (
+    <div
+      className="b
+    lur-sm  z-0 absolute w-full h-full top-0 left-0 grid place-items-center"
+    >
+      <button onClick={() => setIsModalOpen(false)}>X</button>
+      <div className="py-10 w-80 h-64 bg-pink-600 z-10">MODAL</div>
+    </div>
+  );
 };
+
+// const fruits = ["Banana", "Orange", "Lemon"];
+// const citrus = fruits.slice(0, 1);
+// console.log(fruits);
+// console.log(citrus);
+//fruits.slice(-1);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.toSpliced(2, 3, "Lemon", "Kiwi"));
+
+// const months = ["Jan", "Feb", "Mar", "Apr"];
+// console.log(months.toSorted().toReversed());
+
+// const numbers = [4, 9, 16, 25, 29];
+// let first = numbers.find((value) => value > 18);
+
+// function myFunction(value, index, array) {
+//   return value > 18;
+// }
+
+// console.log(first);

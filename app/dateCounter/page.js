1b0631908d8cbba0da4d7c80a30c51c0789ccb7d
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-09 12:19:20 +0100

    date counter page

---
+++ app/dateCounter/page.js
@@ -0,0 +1,10 @@
+import GoBack from "../_components/GoBack";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>DATE COUNTER</h1>
+      <GoBack />
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -21,6 +21,9 @@ export default function Page() {
         <li className="hover:text-cyan-600">
           <Link href="/textExpander">Text Expander</Link>
         </li>
+        <li className="hover:text-cyan-600">
+          <Link href="/dateCounter">Date Counter</Link>
+        </li>
       </ul>
     </div>
   );

7580b8a070bdfc6e7bcc25973344dc9e51f38240
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-16 11:33:41 +0100

    inputs page and links

--- app/inputs/page.js
+++ app/inputs/page.js
@@ -1,10 +1,14 @@
+import Link from "next/link";
 import GoBack from "../_components/GoBack";
 
 export default function Page() {
   return (
-    <div>
-      <h1>INPUTS PAGE</h1>
+    <div className="px-14 py-10">
       <GoBack />
+      <div className="flex gap-4">
+        <Link href="/inputs/form">Form</Link>
+        <Link href="/inputs/range">Range</Link>
+      </div>
     </div>
   );
 }

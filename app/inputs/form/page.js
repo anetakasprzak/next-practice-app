60ee55a4eaa792c8546b044050c44bfd2d6f6229
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-17 11:02:01 +0100

    input form basic styles - tailwindcss

--- app/inputs/form/page.js
+++ app/inputs/form/page.js
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form";
 
 export default function Page() {
   return (
-    <div>
+    <div className="flex flex-col gap-6 items-center mt-20">
       <GoBack />
       <Form />
     </div>
@@ -35,35 +35,49 @@ function Form() {
 
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+        <div className="flex flex-col">
           <input
             type="text"
             placeholder="first name"
             {...register("firstName", { required: true })}
+            className="border border-blue-800 px-1"
           />
-          {errors.firstName && <span>This field is required</span>}
+          {errors.firstName && (
+            <span className="text-xs text-red-600">This field is required</span>
+          )}
         </div>
 
-        <div>
+        <div className="flex flex-col">
           <input
             type="text"
             placeholder="last name"
-            {...register("firstName", { required: true })}
+            {...register("lastName", { required: true })}
+            className="border border-blue-800 px-1"
           />
-          {errors.lastName && <span>This field is required</span>}
+          {errors.lastName && (
+            <span className="text-xs text-red-600">This field is required</span>
+          )}
         </div>
 
-        <div>
+        <div className="flex flex-col">
           <input
             type="text"
             placeholder="email"
             {...register("email", { required: true })}
+            className="border border-blue-800 px-1"
           />
-          {errors.email && <span>This field is required</span>}
+          {errors.email && (
+            <span className="text-xs text-red-600">This field is required</span>
+          )}
         </div>
 
-        <button type="submit">submit</button>
+        <button
+          type="submit"
+          className="hover:text-green-600 border hover:border-green-600"
+        >
+          submit
+        </button>
       </form>
     </div>
   );

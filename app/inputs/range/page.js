237ccd8a88fc53dd9006c8c42f28da49579fa7fc
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-21 19:49:28 +0100

    input range styles

--- app/inputs/range/page.js
+++ app/inputs/range/page.js
@@ -10,10 +10,11 @@ export default function Page() {
   const storageLeft = 1000 - value;
 
   return (
-    <div>
+    <div className="w-[500px] flex flex-col items-center">
       <GoBack />
       <p>
-        You&apos;ve used <span>{storageUsed}</span> of your storage
+        You&apos;ve used <span className="font-semibold">{storageUsed}</span> of
+        your storage
       </p>
       <input
         type="range"
@@ -22,15 +23,16 @@ export default function Page() {
         id="storage"
         value={value}
         onChange={(e) => setValue(e.target.value)}
+        className="w-[300px]"
       />
-      <div>
+      <div className="flex w-[300px] justify-between">
         <label htmlFor="storage">0 GB</label>
         <label htmlFor="storage">1000 GB</label>
       </div>
-      {storageLeft === 0 && <p>NO STORAGE LEFT</p>}
+      {storageLeft === 0 && <p className="uppercase">no storage left</p>}
       {storageLeft > 0 && (
         <p>
-          <span>{storageLeft}</span> gb left
+          <span className="font-semibold">{storageLeft}</span> gb left
         </p>
       )}
     </div>

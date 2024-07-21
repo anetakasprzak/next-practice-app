21b67fe7bf022ed4fe8a4076a34e29fa7d054109
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-21 18:43:41 +0100

    input range jsx

--- app/inputs/range/page.js
+++ app/inputs/range/page.js
@@ -1,3 +1,20 @@
+import GoBack from "@/app/_components/GoBack";
+
 export default function Page() {
-  return <h1>RANGE PAGE</h1>;
+  return (
+    <div>
+      <GoBack />
+      <p>
+        You&apos;ve used <span>X</span> of your storage
+      </p>
+      <input type="range" min="0" max="1000" id="storage" />
+      <div>
+        <label htmlFor="storage">0 GB</label>
+        <label htmlFor="storage">1000 GB</label>
+      </div>
+      <p>
+        <span>X</span> gb left
+      </p>
+    </div>
+  );
 }

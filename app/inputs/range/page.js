9ca136e60a23df17ccb655df5f56fd7e94563108
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-21 19:18:07 +0100

    displaying value

--- app/inputs/range/page.js
+++ app/inputs/range/page.js
@@ -1,20 +1,38 @@
+"use client";
+
 import GoBack from "@/app/_components/GoBack";
+import { useState } from "react";
 
 export default function Page() {
+  const [value, setValue] = useState(0);
+
+  const storageUsed = value;
+  const storageLeft = 1000 - value;
+
   return (
     <div>
       <GoBack />
       <p>
-        You&apos;ve used <span>X</span> of your storage
+        You&apos;ve used <span>{storageUsed}</span> of your storage
       </p>
-      <input type="range" min="0" max="1000" id="storage" />
+      <input
+        type="range"
+        min="0"
+        max="1000"
+        id="storage"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
       <div>
         <label htmlFor="storage">0 GB</label>
         <label htmlFor="storage">1000 GB</label>
       </div>
-      <p>
-        <span>X</span> gb left
-      </p>
+      {storageLeft === 0 && <p>NO STORAGE LEFT</p>}
+      {storageLeft > 0 && (
+        <p>
+          <span>{storageLeft}</span> gb left
+        </p>
+      )}
     </div>
   );
 }

4da5d335ea62c44f95512c158a714a795768aaf2
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-22 12:04:24 +0100

    shopping list form functionality

--- app/list/page.js
+++ app/list/page.js
@@ -1,10 +1,45 @@
+"use client";
+
+import { useState } from "react";
 import GoBack from "../_components/GoBack";
 
 export default function Page() {
+  const [items, setItems] = useState([]);
+  const [newItemName, setNewItemName] = useState("");
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: itemName,
+    };
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!newItemName) return;
+
+    addItem(newItemName);
+    console.log(newItemName);
+    setNewItemName("");
+  };
+
   return (
     <div>
-      <h1>LIST PAGE</h1>
       <GoBack />
+      <h1 className="uppercase">shopping list</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="shopping"
+          placeholder="Add item..."
+          value={newItemName}
+          onChange={(e) => setNewItemName(e.target.value)}
+        />
+        <button>add</button>
+      </form>
+      <ul></ul>
     </div>
   );
 }

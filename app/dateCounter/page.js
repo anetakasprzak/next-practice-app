f07a76150c939a11e5430cf61a094548541cc301
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-07-22 15:53:02 +0100

    date counter jsx

--- app/dateCounter/page.js
+++ app/dateCounter/page.js
@@ -3,8 +3,25 @@ import GoBack from "../_components/GoBack";
 export default function Page() {
   return (
     <div>
-      <h1>DATE COUNTER</h1>
       <GoBack />
+      <h1>DATE COUNTER</h1>
+      <div>
+        <button>-</button>
+        <span>Step: X</span>
+        <button>+</button>
+      </div>
+
+      <div>
+        <button>-</button>
+        <span>Count: X</span>
+        <button>+</button>
+      </div>
+
+      <p>
+        <span>Today is...</span>
+        <span>date</span>
+      </p>
+      <button>reset</button>
     </div>
   );
 }
